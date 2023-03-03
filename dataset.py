04d22e949673c4c72d9bd0921d58fd38aedd7a2f
Ashwij3 <akumbla@wpi.edu>
2023-03-03 17:46:18 -0500

    dataset.py

--- dataset.py
+++ dataset.py
@@ -1,19 +1,27 @@
 import os
-# import torch
-# from torch.utils.data import Dataset
+import torch
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
 
-# class BasicDataSet(Dataset):
-#     def __init__(self,img_dir,mask_dir):
-#         super().__init__()
-#         self.img_dir = img_dir
-#         self.mask_dir = mask_dir
-#         self.images = os.
+class BasicDataSet(Dataset):
+    def __init__(self,img_dir,mask_dir):
+        super().__init__()
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.images = os.listdir(img_dir)
+
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        image_path = os.path.join(self.img_dir,self.images[index])
+        mask_path = os.path.join(self.mask_dir,self.images[index].replace(".jpg", ".png"))
+        
+        image = np.array(Image.open(image_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"))
+
+        assert image.shape[:2] == mask.shape
+        return image,mask
 
 
-# Get the current working 
-# directory (CWD) 
-cwd = os.getcwd() 
-      
-# Print the current working 
-# directory (CWD) 
-print("Current working directory:", cwd) 
